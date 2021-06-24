edc24b0f1eac7d828f2484dfdbb2c8ddfa2bdcfa
Adam Mooz <adam@mooz.ca>
2021-06-24 09:27:18 -0400

    Applied linter fixes
    - Applied fixes via `yarn lint --fix`

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,23 @@
-import React, { useEffect } from 'react';
-import type { AppProps } from 'next/app';
-import { ThemeProvider } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import theme from '../theme';
+import React, { useEffect } from "react";
+import type { AppProps } from "next/app";
+import { ThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import theme from "../theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
-    useEffect(() => {
-        const jssStyles = document.querySelector('#jss-server-side');
-        if (jssStyles && jssStyles.parentElement) {
-            jssStyles.parentElement.removeChild(jssStyles);
-        }
-    }, []);
+  useEffect(() => {
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles && jssStyles.parentElement) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
 
-    return (
-        <ThemeProvider theme={theme}>
-            {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-            <CssBaseline />
-            <Component {...pageProps} />
-        </ThemeProvider>
-    );
+  return (
+    <ThemeProvider theme={theme}>
+      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 export default MyApp;
