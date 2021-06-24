20da6714ad0e20795016c8a4c724f1b771c864b1
Adam Mooz <adam@mooz.ca>
2021-06-24 18:26:01 -0400

    Create Todo modal
    - Implemented a simple add todo modal dialog

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,19 +1,29 @@
-import { Container, AppBar, Toolbar, Typography } from '@material-ui/core';
+import { Container, AppBar, Toolbar, Typography, Button } from '@material-ui/core';
 import { ReactNode } from 'react';
+import { useToggle } from '../../hooks/useToggle';
+import { CreateTodoModal } from '../CreateTodoModal';
 
 interface Props {
   children?: ReactNode;
 }
 
 export function Layout({ children }: Props) {
+  const { value: showCreateTodoModal, toggleValue: toggleCreateTodoModal } = useToggle();
+
   return (
-    <Container>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6">A NextJS-based todo list, by Adam Mooz</Typography>
-        </Toolbar>
-      </AppBar>
-      {children}
-    </Container>
+    <>
+      <Container>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6">A NextJS-based todo list, by Adam Mooz</Typography>
+            <Button color="inherit" onClick={toggleCreateTodoModal}>
+              New
+            </Button>
+          </Toolbar>
+        </AppBar>
+        {children}
+      </Container>
+      <CreateTodoModal open={showCreateTodoModal} onClose={toggleCreateTodoModal} />
+    </>
   );
 }
