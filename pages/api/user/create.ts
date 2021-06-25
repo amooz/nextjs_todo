7167012d5e6f37bcf25b47ceb77feead51f54f7b
Adam Mooz <adam@mooz.ca>
2021-06-25 16:46:53 -0400

    Remove unused user utils
    - They're not used...no need for them!

--- pages/api/user/create.ts
+++
@@ -1,6 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-  let nextId = 0;
-  res.status(200).json({ id: nextId++ });
-};
