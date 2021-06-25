b60f57c8b0d69b4d3d85d23d90cb3ee09a33de92
Adam Mooz <adam@mooz.ca>
2021-06-25 17:43:39 -0400

    Sort by due date
    - Sort the todo's by their due date

--- pages/api/services/todo/getter.ts
+++ pages/api/services/todo/getter.ts
@@ -4,20 +4,23 @@ import { TodoService } from './_todoService';
 export class TodoGetter extends TodoService {
   public get() {
     const promise = new Promise<Result>((resolve, reject) => {
-      this.db.find({}, {}, (error, results) => {
-        if (error) {
-          reject({
-            status: 500,
-            data: {},
-            error,
-          });
-        } else {
-          resolve({
-            status: 200,
-            data: results,
-          });
-        }
-      });
+      this.db
+        .find({}, {})
+        .sort({ dueDate: 1 })
+        .exec((error, results) => {
+          if (error) {
+            reject({
+              status: 500,
+              data: {},
+              error,
+            });
+          } else {
+            resolve({
+              status: 200,
+              data: results,
+            });
+          }
+        });
     });
 
     return promise;
