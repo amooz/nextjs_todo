e9688b846e5f04a08f05ae4cd53718558b7ae990
Adam Mooz <adam@mooz.ca>
2021-06-27 16:11:21 -0400

    Search API
    - can search for todos by name & description

--- pages/api/todo/get.ts
+++ pages/api/todo/get.ts
@@ -1,10 +1,24 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { TodoGetter } from '../services/todo/getter';
 
-const GetTodos = async (_req: NextApiRequest, res: NextApiResponse) => {
+const GetTodos = async (req: NextApiRequest, res: NextApiResponse) => {
   const todoGetter = new TodoGetter();
-  const { status, data, error } = await todoGetter.get();
+  const searchParams = parseSearchParams(req);
+  const { status, data, error } = await todoGetter.get(searchParams);
   res.status(status).json({ data, error });
 };
+
+function parseSearchParams(req: NextApiRequest) {
+  const { search } = req.query;
+  if (!search) {
+    return;
+  }
+
+  if (Array.isArray(search)) {
+    return search.join(', ');
+  }
+
+  return search;
+}
 
 export default GetTodos;
