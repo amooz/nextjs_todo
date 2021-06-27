fa4e24cd4037bf2f2306fcbb43db8d96b429b605
Adam Mooz <adam@mooz.ca>
2021-06-27 16:11:21 -0400

    Search API
    - can search for todos by name & description

--- pages/api/services/todo/getter.ts
+++ pages/api/services/todo/getter.ts
@@ -1,11 +1,12 @@
 import { Result } from '../../../../src/types/result';
 import { TodoService } from './_todoService';
-
 export class TodoGetter extends TodoService {
-  public get() {
+  public get(search?: string) {
+    const query = this.buildQuery(search);
+
     const promise = new Promise<Result>((resolve, reject) => {
       this.db
-        .find({}, {})
+        .find(query, {})
         .sort({ status: -1, dueDate: 1 })
         .exec((error, results) => {
           if (error) {
@@ -24,5 +25,16 @@ export class TodoGetter extends TodoService {
     });
 
     return promise;
+  }
+
+  private buildQuery(search?: string) {
+    if (!search) {
+      return {};
+    }
+
+    const regex = new RegExp(search);
+    return {
+      $or: [{ title: regex }, { description: regex }],
+    };
   }
 }
